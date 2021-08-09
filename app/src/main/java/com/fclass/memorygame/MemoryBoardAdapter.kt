02fe8a6258e4981bf6ecebc1ed8990fe6582ca1d
Raffay Syed <raffay@raffays-air.myfiosgateway.com>
2021-08-08 23:59:57 -0400

    Started Implementing MemoryBoardAdapter Class

---
+++ app/src/main/java/com/fclass/memorygame/MemoryBoardAdapter.kt
@@ -0,0 +1,25 @@
+package com.fclass.memorygame
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
+RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
