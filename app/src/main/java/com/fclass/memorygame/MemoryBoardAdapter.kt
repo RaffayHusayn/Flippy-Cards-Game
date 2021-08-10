6f31cf7d7b1e4566cf41815a46e96ea1ad960d15
Raffay Syed <raffayhusayn@gmail.com>
2021-08-10 04:51:57 -0400

    Creating Some space between different cards in 4x2 grid by setting margins to CARDVIEW as well as adding top and bottom margin to RECYCLERVIEW

--- app/src/main/java/com/fclass/memorygame/MemoryBoardAdapter.kt
+++ app/src/main/java/com/fclass/memorygame/MemoryBoardAdapter.kt
@@ -12,11 +12,20 @@ import kotlin.math.min
 class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
+    //companion object is similar to static variable in java that we don't need an instance of an object
+    //to call it, it can be evoked directly from the class without an object first being created
+    companion object {
+        private const val MARGIN_SIZE = 10
+    }
+
+
+
+
 
    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-       val cardWidth = parent.width / 2 //hardcoding for 2 columns, parent is the RecyclerView
-       val cardHeight  = parent.height / 4 //hardcoding for 4 rows, parent is the RecyclerView
+       val cardWidth = parent.width / 2 - (2*MARGIN_SIZE)//hardcoding for 2 columns, parent is the RecyclerView
+       val cardHeight  = parent.height / 4 - (2*MARGIN_SIZE)//hardcoding for 4 rows, parent is the RecyclerView
        //to make the card square, just take the smaller measurement
        val cardSideLength = min(cardWidth, cardHeight)
 
@@ -24,10 +33,12 @@ class MemoryBoardAdapter(private val context: Context, private val numPieces: In
 
 
        //grabbing out the cardView from the view that we have inflated in LayoutInflater
-       val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams
+       val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
        //dynamically setting the dimension of the card, both same bc we want it to be a square
        layoutParams.height = cardSideLength
        layoutParams.width = cardSideLength
+       //setting viewGroup.MarginLayoutParam allows us to set the margin
+       layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
 
 
        return ViewHolder(view)
