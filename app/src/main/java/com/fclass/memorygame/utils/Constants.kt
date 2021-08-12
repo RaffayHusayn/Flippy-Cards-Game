51b9d9e4b06d7d6465f68e356d510673bd687817
Raffay Syed <raffayhusayn@gmail.com>
2021-08-11 23:50:02 -0400

    Added the vector graphics in a Kotlin class Constants.kt

--- app/src/main/java/com/fclass/memorygame/utils/Constants.kt
+++ app/src/main/java/com/fclass/memorygame/utils/Constants.kt
@@ -1,4 +1,17 @@
 package com.fclass.memorygame.utils
+import com.fclass.memorygame.R
 
-class Constants {
-}
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_android,
+    R.drawable.ic_bulb,
+    R.drawable.ic_circle,
+    R.drawable.ic_cloud,
+    R.drawable.ic_face,
+    R.drawable.ic_fingerprint,
+    R.drawable.ic_fire,
+    R.drawable.ic_flash,
+    R.drawable.ic_leaf,
+    R.drawable.ic_mountain,
+    R.drawable.ic_thumbdown,
+    R.drawable.ic_verified
+    )
