0261f027fa126e7323c2defa3c2e5c438f7cdd72
Raffay Syed <raffayhusayn@gmail.com>
2021-08-11 20:37:31 -0400

    Created a new package and Kotlin file to save the specifications that'll later be used to dynamically change the card grid

---
+++ app/src/main/java/com/fclass/memorygame/models/BoardSize.kt
@@ -0,0 +1,23 @@
+package com.fclass.memorygame.models
+
+enum class BoardSize(val numCards : Int) {
+    EASY(8), //2x4 grid
+    MEDIUM(18),//3x6 grid
+    HARD(24)//4x6 grid
+
+    fun getWidth() : Int{
+        return when (this){
+            EASY -> 2
+            MEDIUM -> 3
+            HARD -> 4
+        }
+    }
+
+    fun getHeight() : Int {
+        return numCards/getWidth()
+    }
+
+    fun getNumPairs() : Int {
+        return numCards / 2
+    }
+}
